5687f3cea33245921bb2144658b678246ddb433f
zloysalat <jurkiw.misha@gmail.com>
2014-08-11 19:56:12 +0300

    Create new project with GoogleMap

---
+++ src/com/yurkiv/wckiev/MainActivity.java
@@ -0,0 +1,241 @@
+package com.yurkiv.wckiev;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity implements  LocationListener {
+
+	private static final String LOG_TAG = "WCKiev";	 
+    private static final String SERVICE_URL = "https://api.myjson.com/bins/21vps";
+	
+	// Google Map
+    private GoogleMap googleMap;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		// Getting Google Play availability status
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+ 
+        // Showing status
+        if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
+ 
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+        } else {  // Google Play Services are available
+        	try {
+                // Loading map
+                initilizeMap();
+     
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }		
+		
+	}
+
+	
+	/**
+     * function to load map. If map is not created it will create it for you
+     * */
+    
+	private void initilizeMap() {
+        if (googleMap == null) {
+        	SupportMapFragment mapFragment = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map);
+        	googleMap = mapFragment.getMap(); 
+        	
+        	// Enabling MyLocation Layer of Google Map
+            googleMap.setMyLocationEnabled(true);
+ 
+            // Getting LocationManager object from System Service LOCATION_SERVICE
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+ 
+            // Creating a criteria object to retrieve provider
+            Criteria criteria = new Criteria();
+ 
+            // Getting the name of the best provider
+            String provider = locationManager.getBestProvider(criteria, true);
+ 
+            // Getting Current Location
+            Location location = locationManager.getLastKnownLocation(provider);
+ 
+            if(location!=null){
+                onLocationChanged(location);
+            }
+            locationManager.requestLocationUpdates(provider, 20000, 0, this);
+        	
+            if (googleMap != null) {
+                setUpMap();
+            }
+        	 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }        
+    }
+	
+	private void setUpMap() {
+        // Retrieve the city data from the web service
+        // In a worker thread since it's a network operation.
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    retrieveAndAddCities();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Cannot retrive cities", e);
+                    return;
+                }
+            }
+        }).start();
+    }
+ 
+	protected void retrieveAndAddCities() throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            // Connect to the web service
+            URL url = new URL(SERVICE_URL);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+ 
+            // Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to service", e);
+            //throw new IOException("Error connecting to service", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+ 
+        // Create markers for the city data.
+        // Must run this on the UI thread since it's a UI operation.
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try {
+                    createMarkersFromJson(json.toString());
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "Error processing JSON", e);
+                }
+            }
+        });
+    }
+	
+	void createMarkersFromJson(String json) throws JSONException {
+        // De-serialize the JSON string into an array of city objects
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            // Create a marker for each city in the JSON data.
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            googleMap.addMarker(new MarkerOptions()
+                .title(jsonObj.getString("name"))
+                .snippet(Integer.toString(jsonObj.getInt("population")))
+                .position(new LatLng(
+                        jsonObj.getJSONArray("latlng").getDouble(0),
+                        jsonObj.getJSONArray("latlng").getDouble(1)
+                 ))
+            );
+        }
+    }
+	
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// Getting latitude of the current location
+        double latitude = location.getLatitude();
+ 
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+ 
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+ 
+        // Showing the current location in Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+ 
+        // Zoom in the Google Map
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+	}
+	
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
